d98d2c248f3c49fad2f13368bfd51757fc15896a
VPraharsha03 <56556436+VPraharsha03@users.noreply.github.com>
2021-06-20 13:48:00 +0530

    Profile for Windows Terminal with emoji support

---
+++ Profiles/Microsoft.PowerShell_wtprofile.ps1
@@ -0,0 +1,20 @@
+Import-Module posh-git
+Import-Module oh-my-posh
+Import-Module -Name Terminal-Icons
+Import-Module Terminal-Icons
+Set-Theme Paradox
+
+if ($host.Name -eq 'ConsoleHost')
+{
+    Import-Module PSReadLine
+}
+
+#Pertain gsudo to current profile's context
+#Function to call the alias with parameters
+function gsudo_pwsh()
+{
+	gsudo.exe powershell.exe -nologo -noexit -noprofile -f $env:USERPROFILE\Documents\WindowsPowerShell\Microsoft.PowerShell_wtprofile.ps1
+}
+ 
+#set alias
+set-alias gsudo gsudo_pwsh
