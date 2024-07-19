47f63649c2a8545fefee9ab4e4c2ac6da937f2cc
BinaryEndeavours <>
2024-07-19 21:07:44 +0530

    Update gruvbox 🌈 theme & clink scripts

---
+++ clink/env.bat
@@ -0,0 +1,4 @@
+setx FZF_CTRL_T_COMMAND "dirx.exe /b /s /X:d /a:-s-h --bare-relative --icons=always --utf8 $dir"
+setx FZF_ALT_C_COMMAND "dirx.exe /b /s /X:d /a:d-s-h --bare-relative --icons=always --utf8 $dir"
+setx FZF_ICON_WIDTH 2
+setx DIRXCMD --icons
--- clink/oh-my-posh.lua
+++ clink/oh-my-posh.lua
@@ -1,1 +1,3 @@
-load(io.popen('oh-my-posh init cmd --config %POSH_THEMES_PATH%/gruvbox-rainbow.omp.json'):read("*a"))()
+load(io.popen('oh-my-posh init cmd --config %POSH_THEMES_PATH%/gruvbox-rainbow.json'):read("*a"))()
+os.execute('fastfetch --logo %USERPROFILE%/.config/fastfetch/ascii.txt')
+os.execute('doskey /macrofile=%LOCALAPPDATA%/clink/aliases')
